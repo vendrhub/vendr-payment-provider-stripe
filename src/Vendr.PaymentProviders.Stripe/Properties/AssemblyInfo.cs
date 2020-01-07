2155f3420c832145d3d267a717451d61109113b7
mattbrailsford <me@mattbrailsford.com>
2020-01-07 15:02:16 +0000

    Included VersionInfo file in project

--- src/Vendr.PaymentProviders.Stripe/Properties/AssemblyInfo.cs
+++ src/Vendr.PaymentProviders.Stripe/Properties/AssemblyInfo.cs
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("54d75f3d-bd42-45e8-bc99-210a9ad71ceb")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
